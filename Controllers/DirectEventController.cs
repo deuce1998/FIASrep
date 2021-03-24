c27868c95924d843d11d88f2b8dd91f75de34001
Alexey <49403307+deuce1998@users.noreply.github.com>
2021-03-24 23:26:46 +0300

    Добавьте файлы проекта.

---
+++ Controllers/DirectEventController.cs
@@ -0,0 +1,31 @@
+﻿using Ext.Net;
+using Ext.Net.Core;
+using FIASrep.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MojeeIO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIASrep.Controllers
+{
+    public class DirectEventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ButtonClick()
+        {
+            var msg = $"Server Time is { DateTime.Now.ToString("H:mm:ss tt") } :+1:";
+
+            this.X().Toast(Mojee.Replace(msg));
+
+            return this.Direct();
+        }
+    }
+}
---
+++ Controllers/GridPanelController.cs
@@ -0,0 +1,21 @@
+﻿using Ext.Net;
+using Ext.Net.Core;
+using FIASrep.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIASrep.Controllers
+{
+    public class GridPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new GridPanelModel());
+        }
+    }
+}
